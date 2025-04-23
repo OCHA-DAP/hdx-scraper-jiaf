149a3e8f9fd7b3363a5ae30723a38ec4f8fd43f0
baripembo <erikawei@gmail.com>
2025-04-22 22:53:46 -0400

    fix NaN values

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import json
-import math
+import numpy as np
 import pandas as pd
 
 def set_header(df):
@@ -79,20 +79,11 @@ def process_data(input_file, output_file):
 
     # Merge sheets using merge_key
     merged_df = pd.merge(df1, df2, on='merge_key', how='left')
-    merged_df = merged_df.where(pd.notnull(merged_df), None)
+    #merged_df = merged_df.where(pd.notnull(merged_df), None)
+    merged_df = merged_df.replace({np.nan: None})
     records = merged_df.to_dict(orient='records')
-    # for rec in records:
-    #     rec['sectors'] = {s: rec.pop(s, None) for s in sectors}
     for rec in records:
-        nested = {}
-        for s in sectors:
-            val = rec.pop(s, None)
-            # if it’s a float NaN, switch to None
-            if isinstance(val, float) and math.isnan(val):
-                nested[s] = None
-            else:
-                nested[s] = val
-        rec['sectors'] = nested
+        rec['sectors'] = {s: rec.pop(s, None) for s in sectors}
 
     # Create json output
     with open(output_file, 'w', encoding='utf-8') as f:
